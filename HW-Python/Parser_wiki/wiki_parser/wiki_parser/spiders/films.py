5113ca02f12662c08b9eefd849f865db4e9d3fc9
Ramil Titeev <derol.gym@gmail.com>
2024-02-11 19:55:16 +0300

    remove user-agent from films.py

--- HW-Python/Parser_wiki/wiki_parser/wiki_parser/spiders/films.py
+++ HW-Python/Parser_wiki/wiki_parser/wiki_parser/spiders/films.py
@@ -62,7 +62,3 @@ class FilmsSpider(scrapy.Spider):
 
         yield item
 
-    custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    }
-
